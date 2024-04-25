c329f840eaf063f5b11b4b052496b230e120acb7
Janno Tjarks <janno.tjarks@mailbox.org>
2024-04-26 00:22:38 +0200

    feat: Adds oz-downloader

---
+++ cmd/oz-downloader/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"jannotjarks/on-downloader/internal/auth"
+	"jannotjarks/on-downloader/internal/visiolink"
+	"net/http"
+	"net/http/cookiejar"
+	"os"
+)
+
+func main() {
+	creds := auth.Credentials{
+		Username: os.Getenv("OZ_DOWNLOADER_USERNAME"),
+		Password: os.Getenv("OZ_DOWNLOADER_PASSWORD"),
+	}
+
+	paper := visiolink.GetOstfriesenZeitungMetadata()
+
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{
+		Jar: jar,
+	}
+
+	handler := visiolink.VisiolinkHandler{Client: client, Paper: paper, Creds: creds}
+	handler.RunDownloadRoutine()
+}
